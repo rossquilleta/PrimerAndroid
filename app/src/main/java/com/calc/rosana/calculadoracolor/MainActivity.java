0827116498c2d337fe47f99697781f0ddd13e9fb
Rosana <rossquilleta@gmail.com>
2016-10-18 13:14:58 +0200

    added butter knife and onSavedInstanceState

--- app/src/main/java/com/calc/rosana/calculadoracolor/MainActivity.java
+++ app/src/main/java/com/calc/rosana/calculadoracolor/MainActivity.java
@@ -8,10 +8,11 @@ import android.widget.Button;
 import android.widget.EditText;
 
 import butterknife.BindView;
+import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity {
 
     private static final String CONCATENATE_VALUE = "concatenado";
     private static final String RESULT_VALUE = "resultado";
@@ -25,38 +26,39 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     boolean decimal = false;
     private Operations operation;
 
-    private double concatenado, resultado, concatenado2=0;
+    private double concatenado, resultado=0;
     private String numero="";
 
     @BindView(R.id.numA) EditText visor;
-    private EditText operando;
 
-    private Button uno;
-    private Button dos;
-    private Button tres;
-    private Button cuatro;
+    @BindView(R.id.num1) Button uno;
+    @BindView(R.id.num2) Button dos;
+    @BindView(R.id.num3) Button tres;
+    @BindView(R.id.num4) Button cuatro;
 
-    private Button cinco;
-    private Button seis;
-    private Button siete;
-    private Button ocho;
+    @BindView(R.id.num5) Button cinco;
+    @BindView(R.id.num6) Button seis;
+    @BindView(R.id.num7) Button siete;
+    @BindView(R.id.num8) Button ocho;
 
-    private Button nueve;
-    private Button cero;
-    private Button igual;
-    private Button reset;
-    private Button coma;
+    @BindView(R.id.num9) Button nueve;
+    @BindView(R.id.num0) Button cero;
 
-    private Button sumar;
-    private Button restar;
-    private Button multiplicar;
-    private Button dividir;
+    @BindView(R.id.igual) Button igual;
+    @BindView(R.id.resetear) Button reset;
+    @BindView(R.id.coma) Button coma;
+
+    @BindView(R.id.suma) Button sumar;
+    @BindView(R.id.resta) Button restar;
+    @BindView(R.id.multipli) Button multiplicar;
+    @BindView(R.id.div) Button dividir;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
         if (savedInstanceState != null) {
             concatenado = savedInstanceState.getDouble(CONCATENATE_VALUE, 0);
@@ -65,51 +67,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             if(op > -1)
                 operation = Operations.values()[op];
         }
-
-        //this.visor = (EditText) findViewById(R.id.numA);
-
-        this.cero = (Button) findViewById(R.id.num0);
-        //cero.setOnClickListener(this);
-        this.uno = (Button) findViewById(R.id.num1);
-        uno.setOnClickListener(this);
-
-        this.dos = (Button) findViewById(R.id.num2);
-        dos.setOnClickListener(this);
-        this.tres = (Button) findViewById(R.id.num3);
-        tres.setOnClickListener(this);
-
-        this.cuatro = (Button) findViewById(R.id.num4);
-        cuatro.setOnClickListener(this);
-        this.cinco = (Button) findViewById(R.id.num5);
-        cinco.setOnClickListener(this);
-
-        this.seis = (Button) findViewById(R.id.num6);
-        seis.setOnClickListener(this);
-        this.siete = (Button) findViewById(R.id.num7);
-        siete.setOnClickListener(this);
-
-        this.ocho = (Button) findViewById(R.id.num8);
-        ocho.setOnClickListener(this);
-        this.nueve = (Button) findViewById(R.id.num9);
-        nueve.setOnClickListener(this);
-
-        this.coma = (Button) findViewById(R.id.coma);
-        coma.setOnClickListener(this);
-        this.reset = (Button) findViewById(R.id.resetear);
-        reset.setOnClickListener(this);
-
-        this.sumar = (Button) findViewById(R.id.suma);
-        sumar.setOnClickListener(this);
-        this.restar = (Button) findViewById(R.id.resta);
-        restar.setOnClickListener(this);
-
-        this.multiplicar = (Button) findViewById(R.id.multipli);
-        multiplicar.setOnClickListener(this);
-        this.dividir = (Button) findViewById(R.id.div);
-        dividir.setOnClickListener(this);
-
-        this.igual = (Button) findViewById(R.id.igual);
-        igual.setOnClickListener(this);
 
     }
 
@@ -120,9 +77,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         // Save our own state now
         if(operation != null)
             outState.putSerializable(OPERATION_VALUE, operation.ordinal());
-
-        outState.putSerializable(CONCATENATE_VALUE, concatenado);
-        outState.putSerializable(RESULT_VALUE, resultado);
+            outState.putSerializable(CONCATENATE_VALUE, concatenado);
+            outState.putSerializable(RESULT_VALUE, resultado);
     }
 
     @OnClick(R.id.num0)
@@ -133,140 +89,153 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         concatenado = Double.parseDouble(numero);
     }
 
-    @Override
-    public void onClick(View v) {
-
-       numero = visor.getText().toString();
-
-        switch (v.getId()) {
-
-            /*case R.id.num0:
-                visor.setText(numero + "0");
-                numero = visor.getText().toString();
-                concatenado = Double.parseDouble(numero);
-                break;*/
-
-            case R.id.num1:
-                visor.setText(numero + "1");
-                numero = visor.getText().toString();
-                concatenado = Double.parseDouble(numero);
-                break;
-
-            case R.id.num2:
-                visor.setText(numero + "2");
-                numero = visor.getText().toString();
-                concatenado = Double.parseDouble(numero);
-                break;
-
-            case R.id.num3:
-                visor.setText(numero + "3");
-                numero = visor.getText().toString();
-                concatenado = Double.parseDouble(numero);
-                break;
-
-            case R.id.num4:
-                visor.setText(numero + "4");
-                numero = visor.getText().toString();
-                concatenado = Double.parseDouble(numero);
-                break;
-
-            case R.id.num5:
-                visor.setText(numero + "5");
-                numero = visor.getText().toString();
-                concatenado = Double.parseDouble(numero);
-                break;
-
-            case R.id.num6:
-                visor.setText(numero + "6");
-                numero = visor.getText().toString();
-                concatenado = Double.parseDouble(numero);
-                break;
-
-            case R.id.num7:
-                visor.setText(numero + "7");
-                numero = visor.getText().toString();
-                concatenado = Double.parseDouble(numero);
-                break;
-
-            case R.id.num8:
-                visor.setText(numero + "8");
-                numero = visor.getText().toString();
-                concatenado = Double.parseDouble(numero);
-                break;
-
-            case R.id.num9:
-                visor.setText(numero + "9");
-                numero = visor.getText().toString();
-                concatenado = Double.parseDouble(numero);
-                break;
-
-            case R.id.coma:
-                decimal=true;
-                if (decimal) {
-                    visor.setText(numero + ".");
-                    concatenado = Double.parseDouble(numero);
-                    decimal = false;
-                } else return;
-                break;
-
-            case R.id.suma:
-                operation = Operations.ADD;
-                resultado=concatenado;
-                visor.setText("");
-                decimal = false;
-                break;
-
-
-            case R.id.resta:
-                operation = Operations.SUBSTRACT;
-                resultado=concatenado;
-                visor.setText("");
-                decimal = false;
-                break;
-
-
-            case R.id.multipli:
-                operation = Operations.MULTIPLY;
-                resultado=concatenado;
-                visor.setText("");
-                decimal = false;
-                break;
-
-            case R.id.div:
-                operation = Operations.DIVIDE;
-                resultado=concatenado;
-                visor.setText("");
-                decimal = false;
-                break;
-
-            case R.id.igual:
-                if (operation == Operations.ADD) {
-                    resultado =resultado+concatenado;
-                    visor.setText(""+resultado);
-                } else if (operation == Operations.SUBSTRACT) {
-                    resultado =resultado-concatenado;
-                    visor.setText("" + resultado);
-                } else if (operation == Operations.MULTIPLY) {
-                    resultado = resultado * concatenado;
-                    visor.setText("" + resultado);
-                } else if (operation == Operations.DIVIDE) {
-                    resultado = resultado / concatenado;
-                    visor.setText("" + resultado);
-                }
-                operation = null;
-                break;
-
-            case R.id.resetear:
-                visor.setText("");
-                break;
-
-            default:
-                visor.setText("0");
-
-        }
+    @OnClick(R.id.num1)
+    public void pressNum1() {
+        numero = visor.getText().toString();
+        visor.setText(numero + "1");
+        numero = visor.getText().toString();
+        concatenado = Double.parseDouble(numero);
     }
 
+    @OnClick(R.id.num2)
+    public void pressNum2() {
+        numero = visor.getText().toString();
+        visor.setText(numero + "2");
+        numero = visor.getText().toString();
+        concatenado = Double.parseDouble(numero);
+    }
 
+    @OnClick(R.id.num3)
+    public void pressNum3() {
+        numero = visor.getText().toString();
+        visor.setText(numero + "3");
+        numero = visor.getText().toString();
+        concatenado = Double.parseDouble(numero);
+    }
+
+    @OnClick(R.id.num4)
+    public void pressNum4() {
+        numero = visor.getText().toString();
+        visor.setText(numero + "4");
+        numero = visor.getText().toString();
+        concatenado = Double.parseDouble(numero);
+    }
+
+    @OnClick(R.id.num5)
+    public void pressNum5() {
+        numero = visor.getText().toString();
+        visor.setText(numero + "5");
+        numero = visor.getText().toString();
+        concatenado = Double.parseDouble(numero);
+    }
+
+    @OnClick(R.id.num6)
+    public void pressNum6() {
+        numero = visor.getText().toString();
+        visor.setText(numero + "6");
+        numero = visor.getText().toString();
+        concatenado = Double.parseDouble(numero);
+    }
+
+    @OnClick(R.id.num7)
+    public void pressNum7() {
+        numero = visor.getText().toString();
+        visor.setText(numero + "7");
+        numero = visor.getText().toString();
+        concatenado = Double.parseDouble(numero);
+    }
+
+    @OnClick(R.id.num8)
+    public void pressNum8() {
+        numero = visor.getText().toString();
+        visor.setText(numero + "8");
+        numero = visor.getText().toString();
+        concatenado = Double.parseDouble(numero);
+    }
+
+    @OnClick(R.id.num9)
+    public void pressNum9() {
+        numero = visor.getText().toString();
+        visor.setText(numero + "9");
+        numero = visor.getText().toString();
+        concatenado = Double.parseDouble(numero);
+    }
+
+    @OnClick(R.id.coma)
+    public void pressComa() {
+        numero = visor.getText().toString();
+        decimal = true;
+        if (decimal){
+        visor.setText(numero + ".");
+        numero = visor.getText().toString();
+        concatenado = Double.parseDouble(numero);
+            decimal = false;
+        } else return;
+
+    }
+
+    @OnClick(R.id.suma)
+    public void pressSuma() {
+        numero = visor.getText().toString();
+        operation = Operations.ADD;
+        visor.setText("");
+        resultado=concatenado;
+        decimal=false;
+    }
+
+    @OnClick(R.id.resta)
+    public void pressResta() {
+        numero = visor.getText().toString();
+        operation = Operations.SUBSTRACT;
+        visor.setText("");
+        resultado=concatenado;
+        decimal=false;
+    }
+
+    @OnClick(R.id.multipli)
+    public void pressMult() {
+        numero = visor.getText().toString();
+        operation = Operations.MULTIPLY;
+        visor.setText("");
+        resultado=concatenado;
+        decimal=false;
+    }
+
+    @OnClick(R.id.div)
+    public void pressDiv(){
+        numero = visor.getText().toString();
+        operation = Operations.DIVIDE;
+        visor.setText("");
+        resultado=concatenado;
+        decimal=false;
+    }
+
+    @OnClick(R.id.igual)
+    public void pressIgual(){
+            numero = visor.getText().toString();
+
+            if (operation == Operations.ADD) {
+                resultado = resultado+concatenado;
+                visor.setText(""+resultado);
+            } else if (operation == Operations.SUBSTRACT) {
+                resultado = resultado-concatenado;
+                visor.setText("" + resultado);
+            } else if (operation == Operations.MULTIPLY) {
+                resultado = resultado * concatenado;
+                visor.setText("" + resultado);
+            } else if (operation == Operations.DIVIDE) {
+                resultado = resultado / concatenado;
+                visor.setText("" + resultado);
+            }
+        concatenado = resultado;
+            operation = null;
+        }
+
+
+    @OnClick(R.id.resetear)
+    public void pressReset() {
+                visor.setText("");
+        }
 
 }
 
